f927cafca6071483b6585f73e91e7d65d14eed08
mbisovskyi <mbisovskyi@gmail.com>
2022-11-29 23:52:39 -0500

    Feature: Fonts Connected + NavBar button click animation added

--- App/src/app/components/nav/nav.component.ts
+++ App/src/app/components/nav/nav.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
   constructor() {}
+
+  handleClick(id: string) {
+    let animatedBtn = document.getElementById(id);
+    animatedBtn?.classList.remove('text-hover');
+    animatedBtn?.classList.add('animation-smoke-btn');
+    animatedBtn?.classList.add('color-trans');
+    setTimeout(() => {
+      animatedBtn?.classList.remove('animation-smoke-btn');
+    }, 410);
+    setTimeout(() => {
+      animatedBtn?.classList.remove('color-trans');
+      animatedBtn?.classList.add('text-hover');
+    }, 500);
+  }
 
   ngOnInit(): void {}
 }
